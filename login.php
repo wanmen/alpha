20f0d616513bab73e57fc0e00f3986852bc48bc2
Shao <shaohuan.li@gmail.com>
2013-06-14 22:23:55 +0800

    Updated

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php 
 
-   $app_id = "605dddd8b117428c97369dfb283eb796";
-   $app_secret = "911a006a5079492d8400374b0a76b9b8";
-   $my_url = "http://lincolnnus.github.io/OneManU/login.php";
-   $grant_type="authorization_code"  //支持的授权类型
+   $app_id = "236279";
+   $app_secret = "605dddd8b117428c97369dfb283eb796";
+   $my_url = "http://xiexieshi.com/onemanu/login.php";
+   $grant_type="authorization_code"; //支持的授权类型
 
    session_start();
    $code = $_REQUEST["code"];
@@ -22,9 +22,12 @@
        . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
        . "&client_secret=" . $app_secret ."&grant_type=" . $grant_type . "&code=" . $code;
 
+       echo $token_url;
      $response = @file_get_contents($token_url);
      $params = null;
+     echo $response;
      parse_str($response, $params);
+     var_dump($params);
 
      $graph_url = "https://api.renren.com/v2/user?access_token=" 
        . $params['access_token'];
