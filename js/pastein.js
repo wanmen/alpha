ad367f6443750330d33f731a52edd57df28ec89a
NorthStar <xxu@hmc.edu>
2013-06-25 00:51:45 -0700

    Cleanedup code and adjusted pictures.

--- js/pastein.js
+++ js/pastein.js
@@ -4,11 +4,23 @@
  * Todo: renren sharing and weibo sharing.
  */
 
-var _gaq = _gaq || [];
+/*var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-41886368-1']);
 _gaq.push(['_trackPageview'])
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+})();*/
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-41993161-1']);
+  _gaq.push(['_setDomainName', 'wanmen.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
